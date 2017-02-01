887ee076efb65779da84aa39fa335a079e423a41
Zack Siri <zack@codemy.net>
2017-02-01 15:33:08 +0700

    finish setting up cors

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,16 @@ module Invoiced
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any,
+          methods: [:get, :patch, :put, :delete, :post, :options]
+        )
+      end
+    end
   end
 end
